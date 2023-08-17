31c9509525996b3e587a500798807c6ffae6bbca
Udezi Oghenero <udezioghenero@mail.com>
2023-08-17 19:17:33 +0100

    first file upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# Todo List App
+
+This is a basic Todo List application built using HTML, CSS, and JavaScript. The app allows users to create, manage, and track their tasks.
+
+## Features
+
+- Add new tasks with titles and descriptions.
+- Mark tasks as completed.
+- Delete tasks.
+- Filter tasks by their completion status.
+- Responsive design for various screen sizes.
+
+## Usage
+
+1. Clone the repository:
+
+   
+      git clone https://github.com/Neroshell/check-todo-list-app.git
+
+      Technologies Used
+      HTML
+      CSS
+      JavaScript
+      Contributing
+      Contributions are welcome! If you find any issues or would like to add new features, feel free to create a pull request.
---
+++ index.js
@@ -0,0 +1,78 @@
+var button = document.querySelector(".bot")
+var inputBox = document.querySelector(".input-box")
+var listContainer = document.querySelector(".task-list");
+
+
+button.addEventListener("click", addTask);
+
+
+function addTask(event) {
+    // Change button background color before showing alert
+    
+    this.style.backgroundColor = "#97FEED";
+    
+    setTimeout(function(){
+        button.style.backgroundColor = "#35A29F";
+    },1000);
+
+    if (inputBox.value === "") {
+        alert("Please insert a task");
+    } else {
+        let list = document.createElement("li");
+        list.innerHTML = inputBox.value;
+        listContainer.appendChild(list);
+        let close = document.createElement("SPAN");
+        close.innerHTML = "\u00d7";
+        list.appendChild(close);
+    }
+
+    inputBox.value = "";
+
+    saveData();
+}
+
+
+listContainer.addEventListener("click", play);
+
+function play(event) {
+
+        if (event.target.tagName === "LI"){
+
+            event.target.classList.toggle("checked");
+        }else if (event.target.tagName === "SPAN"){
+            event.target.parentElement.remove();
+            
+        }
+       saveData();       
+}
+
+function saveData(){
+
+    localStorage.setItem("list", listContainer.innerHTML);
+}
+
+function displayData(){
+
+   listContainer.innerHTML = localStorage.getItem("list");
+}
+
+displayData();
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    let bar = document.querySelector("#bar");
+    bar.addEventListener("click", openNav);
+
+    function openNav() {
+    
+        document.getElementById("mySidenav").style.width = "100%";
+    }
+
+    let close = document.getElementById("closeNav");
+    close.addEventListener("click", closeNav);
+
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    }
+});
+
